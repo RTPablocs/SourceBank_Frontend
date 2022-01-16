6b2238913790f6a9136955de3ae42f3763286f2b
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-01-17 00:35:22 +0100

    Tailwind initialization

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    important: true,
+    darkMode: "class",
+    i18n: {locales: ["en-US"], defaultLocale: "en-US"},
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                check: "url('/icons/check.svg')",
+                landscape: "url('/images/landscape/2.jpg')"
+            }),
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ["checked"],
+            borderColor: ["checked"],
+            inset: ["checked"],
+            zIndex: ["hover", "active"]
+        },
+    },
+    plugins: [],
+    future: {
+        purgeLayersByDefault: true
+    },
+};
