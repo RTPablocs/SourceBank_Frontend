c22a9761ce2e61181667747668129b95264d21ae
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-01-18 22:22:57 +0100

    tailwind config update, added lucide-react and solved issue with mini-css-extract-plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
     important: true,
     darkMode: "class",
-    i18n: {locales: ["en-US"], defaultLocale: "en-US"},
+    i18n: {
+        locales: ["en-US"],
+        defaultLocale: "en-US"
+    },
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
             backgroundImage: (theme) => ({
-                check: "url('/icons/check.svg')",
-                landscape: "url('/images/landscape/2.jpg')"
             }),
         },
     },
