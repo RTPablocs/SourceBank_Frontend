11c02c10709a8060717626057c7a4de990447b02
RTPablocs <conejerosorianopablo@gmail.com>
2022-01-31 12:28:46 +0100

    Added DashMainCard component

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,7 +1,9 @@
+import DashMainCard from "./DashMainCard";
+
 export default function DashboardLayout() {
     return (
-        <div>
-
+        <div className='grid grid-cols-2'>
+        <DashMainCard balance={10000}/>
         </div>
     )
 }
