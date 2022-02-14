8efc55a6816efba987c91e0abf5febdc12107cf1
Pablo Conejero <conejerosorianopablo@gmail.com>
2022-02-14 16:37:09 +0100

    Added VaultModal basic structure

---
+++ src/components/VaultModal.jsx
@@ -0,0 +1,9 @@
+
+/* TODO implement useIonModal as shown in the docs, remember that This is only appearance,
+     and must not contain nothing about the hook here
+*/
+export default function VaultModal(){
+    return(
+        <></>
+    )
+}
