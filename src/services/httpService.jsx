1f84d56cc644102b2f38e118eabdb06c8f63443d
Pablo Conejero Soriano <conejerosorianopablo@gmail.com>
2022-05-20 09:05:56 +0200

    added .env

--- src/services/httpService.jsx
+++ src/services/httpService.jsx
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default function HttpService(isLogged = false) {
     return isLogged === true ? axios.create({
-        baseURL: 'http://10.42.0.1:8000',
+        baseURL: `http://${process.env.REACT_APP_BASE_URL}`,
         headers: {
             'Authorization': `Bearer ${localStorage.getItem('auth')}`
         }
     }) : axios.create({
-        baseURL: 'http://10.42.0.1:8000'
+        baseURL: `http://${process.env.REACT_APP_BASE_URL}`
     })
 }
